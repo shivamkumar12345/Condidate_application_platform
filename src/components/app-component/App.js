7a124f98afb3e93f6de83ce7d1960932c6252326
shivamkumar12345 <sk9220449@gmail.com>
2024-05-06 10:48:22 +0530

    added filter functionality

--- src/components/app-component/App.js
+++ src/components/app-component/App.js
@@ -3,8 +3,11 @@ import { CircularProgress, OutlinedInput } from '@mui/material'
 import "./App.css"
 import getData from "../../services/api-service"
 import JobCard from '../job-card/JobCard'
+import { getValueInRs } from '../../services/filter-service'
 import FilterType from '../filter-type/FilterType'
 const loadedData =[];
+const appliedFilter = new Map();
+
 const App = () => {
   const [jobList,setJobList] = useState([]);
   const [isLoading,setLoading] = useState(true);
@@ -12,13 +15,15 @@ const App = () => {
   const [offset, setOffset] = useState(0);
   const [companyName,setCompanyName] = useState("");
  
-
+/**
+ * used for api call upon change of offset variable
+ */
   useEffect(() => {
     async function fetchData() {
       try{
         const data = await getData(offset);
         loadedData.push(...data)
-        setJobList(loadedData);
+        onFilterSelect();
         setLoading(false);
       }catch(er){
         setError("Error "+ er.toString());
@@ -28,8 +33,18 @@ const App = () => {
     }
     fetchData();
   }, [offset])
+ 
+//added debounced effect for search functionality
+useEffect(()=>{
+    const timeoutId = setTimeout(() => {
+      onFilterSelect('companyName',companyName);
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  },[companyName])
 
-
+  /**
+ * used for infinite scroll event to change the offset
+ */
   async function handleScrollEvent(event){
     
     const scrollHeight = document.documentElement.scrollHeight;
@@ -43,12 +58,16 @@ const App = () => {
     }
   }
 
+  /**
+ * event listner for scroll event
+ */
   useEffect(()=>{
       window.addEventListener("scroll",handleScrollEvent);
       return ()=> window.removeEventListener("scroll",handleScrollEvent);
   },[]);
 
-  
+ 
+  //show loading bar when we open the oage for the first time
   if(isLoading && jobList.length === 0){
     return <div className='Loading'>
             <CircularProgress />
@@ -59,9 +78,68 @@ const App = () => {
       {error}
     </div>
   }
+
+  /**
+ * filter function will get call 
+ * for every filter type and selection change
+ */
   function onFilterSelect(type,selectedOptions){
-    console.log(type + " " + selectedOptions);
-  }
+   
+    appliedFilter.set(type,selectedOptions);
+
+    const filteredList =loadedData.filter(item=>{
+      let shouldFilterOut=true;
+
+      appliedFilter.forEach((options,key)=>{
+        if(options){
+          switch(key){
+            case "Min experience":
+              if(item['minExp'] && options && Number(item.minExp) >= Number(options.value)){
+                shouldFilterOut &=true;
+              }else{
+                shouldFilterOut =false;
+              }
+              break;
+            
+              case "Min base pay":
+                const minExpectedSalary = Number(item.minJdSalary) * getValueInRs(item.salaryCurrencyCode);
+                if(item['minJdSalary'] && options && minExpectedSalary >= Number(options.value)){
+                  shouldFilterOut &=true;
+                }else{
+                  shouldFilterOut =false;
+                }
+                break;
+              
+              case "Job role":
+                if(!item['jobRole']){
+                  shouldFilterOut =false;
+                }else{
+                  shouldFilterOut &= options.length==0 || options.filter(filter => item['jobRole'].toLowerCase() === filter.value ).length  >0;
+                }
+                break;
+              
+              case "Remote":
+              case "location":
+                if(!item['location']){
+                  shouldFilterOut = false;
+                }else{
+                  shouldFilterOut &= options.length == 0 || options.filter(filter => item['location'].includes(filter.value) ).length  >0;
+                }
+              case 'companyName':
+                if(!item[key]){
+                  shouldFilterOut = false;
+                }else{
+                  shouldFilterOut &= item[key].toLowerCase().includes(options.toLowerCase());
+                }
+
+          }    
+        }  
+      })
+      return shouldFilterOut;
+     
+    });
+    setJobList(filteredList);
+}
 
   return (
     <>
